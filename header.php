6986c951096519e4ec1b93e1d064b0573ac457f6
OlgaMahryrtska <42007610+OlgaMahryrtska@users.noreply.github.com>
2020-05-21 11:07:49 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<div class="header" style="position: fixed">
+    <a href='#'></a><?php the_custom_logo(); ?>
+    <!--    <h2 class="logo">LOGO</h2>-->
+
+    <ul class="menu">
+        <a href="#courses">Paid courses</a>
+        <a href="#workshops">Paid workshops</a>
+        <a href="#free">Free-of-charge events</a>
+        <a href="#">About Us</a>
+        <a href="#" id="logIn">Log In</a>
+        <!--
+      <input type="text" id="mySearch"  placeholder="SEARCH.." title="Type in a category">
+  
+      <label for="chk" class="hide-menu-btn">
+        <i class="fas fa-times"></i>
+      </label>
+-->
+    </ul>
+</div>
+<?php wp_head();
+?>
