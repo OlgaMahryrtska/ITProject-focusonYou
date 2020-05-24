489ce9f1afe92cedc4ea488be38794ddee36a3b6
OlgaMahryrtska <42007610+OlgaMahryrtska@users.noreply.github.com>
2020-05-24 23:17:43 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,71 @@
+<?php wp_footer(); ?>
+<?php
+/**
+ * Theme Name: focusonYou
+ *Theme URI:focusonYou.pl
+ *Description: The own theme for wordPress
+ *Authors: Anna Shevchenko, Maria Hodis, Olga Magrytska, Yulia Pavliuk
+ *Version:1
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<footer>
+  <div class="footer_class">
+    <div>
+      <ul class="footer_menu_1">
+        <li>
+          <p>Web page</p>
+        </li>
+        <li><a href="#courses">Paid courses</a></li>
+        <li><a href="#workshops">Paid workshops</a></li>
+        <li><a href="#free">Free-of-charge events</a></li>
+        <li><a href="#">About Us</a></li>
+      </ul>
+    </div>
+    <div>
+      <ul class="footer_menu">
+        <li>
+          <p>Support</p>
+        </li>
+        <li><a href="#requirements">Requirements</a></li>
+        <li><a href="#payment">Payment</a></li>
+      </ul>
+    </div>
+    <div>
+      <ul class="footer_menu">
+        <li>
+          <p>Company</p>
+        </li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#jobs">Jobs</a></li>
+        <li><a href="#press">Press</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </div>
+    <div>
+      <ul class="footer_menu">
+        <li>
+          <p>Terms & Policies</p>
+        </li>
+        <li><a href="#terms">Terms of Service</a></li>
+        <li><a href="#privacy">Privacy Statement</a></li>
+        <li><a href="#security">Security</a></li>
+      </ul>
+    </div>
+    <div>
+      <ul class="footer_menu">
+        <li>
+          <p>Community</p>
+        </li>
+        <li><a href="#meetups">Meetups</a></li>
+        <li><a href="#newsletter">Newsletter</a></li>
+        <li><a href="#fb">Facebook group</a></li>
+        <li><a href="#inst">Instagram profile</a></li>
+      </ul>
+    </div>
+  </div>
+</footer>
+
+
